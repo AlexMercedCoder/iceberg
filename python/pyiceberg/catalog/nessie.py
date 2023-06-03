648147d4b73f3a028b53f82b83e30847e693bace
Alex Merced <alex@alexmerced.dev>
2023-06-03 17:31:06 -0400

    boiler plate for Nessie PyIceberg Catalog Implementation

---
+++ python/pyiceberg/catalog/nessie.py
@@ -0,0 +1,187 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict, Optional, Union
+
+class NessieCatalog(Catalog):
+    def __init__(self):
+        # Initialize any properties here, similar to the Java constructor
+        pass
+
+    def initialize(self, name: str, options: Dict[str, str]):
+        # This method would set up the catalog with the given name and options
+        pass
+    
+    def create_table(
+        self,
+        identifier: Union[str, Identifier],
+        schema: Schema,
+        location: Optional[str] = None,
+        partition_spec: PartitionSpec = UNPARTITIONED_PARTITION_SPEC,
+        sort_order: SortOrder = UNSORTED_SORT_ORDER,
+        properties: Properties = EMPTY_DICT,
+    ) -> Table:
+        """Create a table
+
+        Args:
+            identifier (str | Identifier): Table identifier.
+            schema (Schema): Table's schema.
+            location (str | None): Location for the table. Optional Argument.
+            partition_spec (PartitionSpec): PartitionSpec for the table.
+            sort_order (SortOrder): SortOrder for the table.
+            properties (Properties): Table properties that can be a string based dictionary.
+
+        Returns:
+            Table: the created table instance
+
+        Raises:
+            TableAlreadyExistsError: If a table with the name already exists
+        """
+
+    def load_table(self, identifier: Union[str, Identifier]) -> Table:
+        """Loads the table's metadata and returns the table instance.
+
+        You can also use this method to check for table existence using 'try catalog.table() except NoSuchTableError'
+        Note: This method doesn't scan data stored in the table.
+
+        Args:
+            identifier (str | Identifier): Table identifier.
+
+        Returns:
+            Table: the table instance with its metadata
+
+        Raises:
+            NoSuchTableError: If a table with the name does not exist
+        """
+
+    def drop_table(self, identifier: Union[str, Identifier]) -> None:
+        """Drop a table.
+
+        Args:
+            identifier (str | Identifier): Table identifier.
+
+        Raises:
+            NoSuchTableError: If a table with the name does not exist
+        """
+
+    def rename_table(self, from_identifier: Union[str, Identifier], to_identifier: Union[str, Identifier]) -> Table:
+        """Rename a fully classified table name
+
+        Args:
+            from_identifier (str | Identifier): Existing table identifier.
+            to_identifier (str | Identifier): New table identifier.
+
+        Returns:
+            Table: the updated table instance with its metadata
+
+        Raises:
+            NoSuchTableError: If a table with the name does not exist
+        """
+
+    def create_namespace(self, namespace: Union[str, Identifier], properties: Properties = EMPTY_DICT) -> None:
+        """Create a namespace in the catalog.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier
+            properties (Properties): A string dictionary of properties for the given namespace
+
+        Raises:
+            NamespaceAlreadyExistsError: If a namespace with the given name already exists
+        """
+
+    def drop_namespace(self, namespace: Union[str, Identifier]) -> None:
+        """Drop a namespace.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier
+
+        Raises:
+            NoSuchNamespaceError: If a namespace with the given name does not exist
+            NamespaceNotEmptyError: If the namespace is not empty
+        """
+
+    def list_tables(self, namespace: Union[str, Identifier]) -> List[Identifier]:
+        """List tables under the given namespace in the catalog.
+
+        If namespace not provided, will list all tables in the catalog.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier to search.
+
+        Returns:
+            List[Identifier]: list of table identifiers.
+
+        Raises:
+            NoSuchNamespaceError: If a namespace with the given name does not exist
+        """
+
+    def list_namespaces(self, namespace: Union[str, Identifier] = ()) -> List[Identifier]:
+        """List namespaces from the given namespace. If not given, list top-level namespaces from the catalog.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier to search.
+
+        Returns:
+            List[Identifier]: a List of namespace identifiers
+
+        Raises:
+            NoSuchNamespaceError: If a namespace with the given name does not exist
+        """
+
+    def load_namespace_properties(self, namespace: Union[str, Identifier]) -> Properties:
+        """Get properties for a namespace.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier
+
+        Returns:
+            Properties: Properties for the given namespace
+
+        Raises:
+            NoSuchNamespaceError: If a namespace with the given name does not exist
+        """
+
+    def update_namespace_properties(
+        self, namespace: Union[str, Identifier], removals: set[str] | None = None, updates: Properties = EMPTY_DICT
+    ) -> PropertiesUpdateSummary:
+        """Removes provided property keys and updates properties for a namespace.
+
+        Args:
+            namespace (str | Identifier): Namespace identifier
+            removals (Set[str]): Set of property keys that need to be removed. Optional Argument.
+            updates (Properties): Properties to be updated for the given namespace.
+
+        Raises:
+            NoSuchNamespaceError: If a namespace with the given name does not exist
+            ValueError: If removals and updates have overlapping keys.
+        """
+
+    def name(self) -> str:
+        # This method would return the catalog's name
+        pass
+
+    def new_table_ops(self, table_identifier: str) -> Table:
+        # This method would create a new TableOperations instance for the given table identifier
+        pass
+
+    def default_warehouse_location(self, table: str) -> str:
+        # This method would provide the default warehouse location for a table
+        pass
+
+    def list_tables(self, namespace: str) -> List[Table]:
+        # This method would list all tables in the given namespace
+        pass
+
+    def load_namespace_metadata(self, namespace: str) -> Dict[str, str]:
+        # This method would load metadata for the given namespace
+        pass
+
+    def drop_namespace(self, namespace: str) -> bool:
+        # This method would drop the given namespace
+        pass
+
+    def set_properties(self, namespace: str, properties: Dict[str, str]) -> bool:
+        # This method would set properties for the given namespace
+        pass
+
+    def close(self) -> None:
+        # This method would close any open resources associated with the catalog
+        pass
